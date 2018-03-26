b6226824507c95bc59c57c75ced64dc65b4ff431
JanAnders <36856649+JanAnders@users.noreply.github.com>
2018-03-26 11:42:03 +0200

    Neue Klasse hinzugefügt.

---
+++ HelloGit/src/Jan.java
@@ -0,0 +1,7 @@
+
+public class Jan {
+
+	public void ausgabe() {
+		System.out.println("Jan macht ausgabe");
+	}
+}
---
+++ HelloGit/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Achtung: Klappen die Klassen?");
+		
+		Jan jan = new Jan();
+		jan.ausgabe();
+		//eine änderung
+	}
+}
